9b91b33069c96314e6ec82c87d3f80373a3d8a62
Hrushikesh Borse <hrushikesh@furystallion.in>
2025-07-07 17:50:26 +0530

    ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -26,7 +26,7 @@ resource "aws_instance" "private_ec2" {
   ami                         = var.ec2_ami
   instance_type               = var.ec2_instance_type
   subnet_id                   = aws_subnet.private[0].id
-  vpc_security_group_ids      = [aws_security_group.private_ec2_sg]
+  vpc_security_group_ids      = [aws_security_group.private_ec2_sg.id]
   associate_public_ip_address = false
 
   tags = merge(local.tags, {
