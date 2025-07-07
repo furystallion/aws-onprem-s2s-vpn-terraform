d7ee53cdddfa6cc479d243ca2df4ef4095db953c
Hrushikesh Borse <hrushikesh@furystallion.in>
2025-07-07 17:12:21 +0530

    ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "ec2_sg" {
+resource "aws_security_group" "private_ec2_sg" {
   name        = "ec2-allow-icmp"
   description = "Allow ICMP (ping) from on-prem network"
   vpc_id      = aws_vpc.main.id
@@ -26,7 +26,7 @@ resource "aws_instance" "private_ec2" {
   ami                         = var.ec2_ami
   instance_type               = var.ec2_instance_type
   subnet_id                   = aws_subnet.private[0].id
-  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
+  vpc_security_group_ids      = [aws_security_group.private_ec2_sg]
   associate_public_ip_address = false
 
   tags = merge(local.tags, {
